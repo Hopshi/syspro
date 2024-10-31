c163fc2cdfa6cc7f175e6d1a0330eb6ba8f45b5d
JaminOh <ojmjr0818@gmail.com>
2024-10-31 02:11:25 +0000

    commit: chapt5,6,7

---
+++ chap5/hw1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+int main() {
+	char savedText[10][100];
+	read(fd, &buf, 1);
+	saveText[0][0] = buf;
+	read(fd, &buf, 1);
+	saveText[0][1] = buf;
+}
